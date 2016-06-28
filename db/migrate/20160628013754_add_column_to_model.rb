4a31a138daf38cb6416f0543b3bac97a23ccb1be
nobel <nobelarmy@outlook.com>
2016-06-28 10:00:35 -0500

    heroku test

--- db/migrate/20160628013754_add_column_to_model.rb
+++ db/migrate/20160628013754_add_column_to_model.rb
@@ -1,5 +1,5 @@
 class AddColumnToModel < ActiveRecord::Migration
   def change
-    add_reference :visitas, :contacto, index: true, foreign_key: true
+    add_reference :models, :column, index: true, foreign_key: true
   end
 end
